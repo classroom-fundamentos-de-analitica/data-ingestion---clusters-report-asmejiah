0bb1e94868101f58dd92a77c080c607b8b28ef83
asmejiah <111844634+asmejiah@users.noreply.github.com>
2022-11-26 13:42:04 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,26 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+   df = pd.read_fwf(
+        "clusters_report.txt", widths = [9, 16, 16, 80], header = None,
+        names = ["cluster","cantidad_de_palabras_clave","porcentaje_de_palabras_clave", "-"],
+        skip_blank_lines = False, converters = {"porcentaje_de_palabras_clave": 
+        lambda x: x.rstrip(" %").replace(",",".")}).drop([0,1,2,3], axis=0)
+
+    col4 = df["-"]
+    df = df[df["cluster"].notna()].drop("-", axis=1)
+    df = df.astype({ "cluster": int, "cantidad_de_palabras_clave": int, "porcentaje_de_palabras_clave": float})
+
+    col4Proc = []
+    text = ""
+    for lin in col4:
+        if isinstance(lin, str): text += lin+" "
+        else:
+            text = ", ".join([" ".join(x.split()) for x in text.split(",")])
+            col4Proc.append(text.rstrip("."))
+            text = ""
+            continue
+
+    df["principales_palabras_clave"] = col4Proc
 
     return df
