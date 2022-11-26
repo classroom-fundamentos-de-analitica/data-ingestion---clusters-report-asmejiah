b635e4e4a07127509228a417dbddf41919b660f9
asmejiah <111844634+asmejiah@users.noreply.github.com>
2022-11-26 13:50:06 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -20,20 +20,19 @@ def ingest_data():
         skip_blank_lines = False, converters = {"porcentaje_de_palabras_clave": 
         lambda x: x.rstrip(" %").replace(",",".")}).drop([0,1,2,3], axis=0)
 
-    col4 = df["-"]
-    df = df[df["cluster"].notna()].drop("-", axis=1)
-    df = df.astype({ "cluster": int, "cantidad_de_palabras_clave": int, "porcentaje_de_palabras_clave": float})
+   col4 = df["-"]
+   df = df[df["cluster"].notna()].drop("-", axis=1)
+   df = df.astype({ "cluster": int, "cantidad_de_palabras_clave": int, "porcentaje_de_palabras_clave": float})
 
-    col4Proc = []
-    text = ""
-    for lin in col4:
-        if isinstance(lin, str): text += lin+" "
-        else:
-            text = ", ".join([" ".join(x.split()) for x in text.split(",")])
-            col4Proc.append(text.rstrip("."))
-            text = ""
-            continue
+   col4Proc = []
+   text = ""
+   for lin in col4:
+       if isinstance(lin, str): text += lin+" "
+       else:
+           text = ", ".join([" ".join(x.split()) for x in text.split(",")])
+           col4Proc.append(text.rstrip("."))
+           text = ""
+           continue
+   df["principales_palabras_clave"] = col4Proc
 
-    df["principales_palabras_clave"] = col4Proc
-
-    return df
+   return df
